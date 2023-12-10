f4fdeecc3c3ece877e510bd36343d0b1ed44cff5
Diogo Dias <diogodiasgrilo@gmail.com>
2023-12-09 23:03:39 -0700

    December 9

--- to_sql.py
+++ to_sql.py
@@ -101,7 +101,7 @@ def json_to_sql_project_links(json_file, sql_file_prefix):
                 );
             """
 
-            # Open SQL file for writing (creates it if it doesn't exist)
+            # Open the SQL file for writing (creates it if it doesn't exist)
             with open(sql_file, 'w') as sql_output:
                 sql_output.write(sql_schema)
                 node_id = 0
